6443c76bd3ec26a15a5e71f1e64c455a44f24fc8
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-06-20 13:09:20 +0200

    Add save_df function to utils

--- R/utils.R
+++ R/utils.R
@@ -9,11 +9,11 @@
 #' @export
 #'
 #' @examples
-#' create_directory("my_directory", date = FALSE)
-#' create_directory("my_directory/inner_dir", date = TRUE)
+#' create_dir("my_directory", date = FALSE)
+#' create_dir("my_directory/inner_dir", date = TRUE)
 #' # Clean up the created directory
 #' unlink("my_directory", recursive = TRUE)
-create_directory <- function(dir_name, date = FALSE) {
+create_dir <- function(dir_name, date = FALSE) {
 
   if (date) {
     current_date <- format(Sys.time(), "%Y_%m_%d_%H%M%S")  # Get the current date and time
@@ -32,6 +32,46 @@ create_directory <- function(dir_name, date = FALSE) {
 
   } else {
     message(paste("Directory", dir_name, "already exists."))
+  }
+
+  invisible(NULL)
+
+}
+
+
+#' Save dataframe in CSV, TSV, or RDA format
+#'
+#' The function saves a dataframe in the specified format (CSV, TSV, or RDA) in the specified directory.
+#'
+#' @param df The dataframe to save
+#' @param dir_name The directory where the file will be saved
+#' @param file_name The name of the file to save
+#' @param file_type The type of file to save the dataframe as. Options are "csv", "tsv", or "rda"
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' df <- data.frame(x = 1:10, y = rnorm(10))
+#' save_df(df, "my_data", "sample_data", "csv")
+#' # Clean up the created directory
+#' unlink("my_data", recursive = TRUE)
+save_df <- function(df, dir_name, file_name, file_type = c("csv", "tsv", "rda")) {
+  file_type <- match.arg(file_type)
+
+  # Create the directory if it doesn't exist, else store the file in the existing directory
+  create_dir(dir_name)
+
+  file_path <- file.path(dir_name, paste0(file_name, ".", file_type))
+
+  if (file_type == "csv") {
+    utils::write.csv(df, file_path, row.names = FALSE)
+  } else if (file_type == "tsv") {
+    utils::write.table(df, file_path, sep = "\t", row.names = FALSE, col.names = TRUE)
+  } else if (file_type == "rda") {
+    save(df, file = file_path)
+  } else {
+    stop("Unsupported file type: ", file_type)
   }
 
   invisible(NULL)
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,13 +1,13 @@
 test_that("Directory creation without date", {
   dir_name <- "test_directory_without_date"
-  create_directory(dir_name)
+  create_dir(dir_name)
   expect_true(dir.exists(dir_name), "Directory should be created")
   unlink(dir_name, recursive = TRUE)
 })
 
 test_that("Directory creation with date", {
   dir_prefix <- "test_directory_with_date"
-  create_directory(dir_prefix, date = TRUE)
+  create_dir(dir_prefix, date = TRUE)
 
   # List all directories that match the prefix
   created_dirs <- list.files(pattern = paste0("^", dir_prefix, "_\\d{4}_\\d{2}_\\d{2}_\\d{6}$"))
@@ -19,14 +19,14 @@ test_that("Directory creation with date", {
 
 test_that("Handling existing directory", {
   dir_name <- "existing_directory"
-  create_directory(dir_name)
-  create_directory(dir_name)  # Attempt to create again
+  create_dir(dir_name)
+  create_dir(dir_name)  # Attempt to create again
   expect_true(dir.exists(dir_name), "Existing directory should still exist")
   unlink(dir_name, recursive = TRUE)
 })
 
 test_that("Warning on failed directory creation", {
   dir_name <- "C:/Windows/System32/restricted_directory"  # A directory that likely requires superuser privileges
-  expect_warning(create_directory(dir_name), "Failed to create directory")
+  expect_warning(create_dir(dir_name), "Failed to create directory")
   unlink(dir_name, recursive = TRUE)
 })
