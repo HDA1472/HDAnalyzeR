cef7a8e5cdaad0eeffa0c4280ad39bdb03e3d219
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-06-24 10:02:47 +0200

    Add impute_median() tests

--- tests/testthat/test-impute.R
+++ tests/testthat/test-impute.R
@@ -1,3 +1,42 @@
+# Test impute_median -----------------------------------------------------------
+test_that("impute_median imputes values in the proper way", {
+  test_data <- tibble::tibble(
+    A = c(80, 44, NA, 50, 29),  # The median of the 1st col are 47
+    B = c(30, NA, 85, 70, 54),  # The median of the 2nd col are 62
+    C = c(7, 10, 25, 74, 49),
+    D = c(14, 0, 5, 9, 20)
+  )
+  result <- impute_median(test_data)
+  expected <- tibble::tibble(
+    A = c(80, 44, 47, 50, 29),
+    B = c(30, 62, 85, 70, 54),
+    C = c(7, 10, 25, 74, 49),
+    D = c(14, 0, 5, 9, 20)
+  )
+  expect_equal(result, expected)
+})
+
+
+test_that("impute_median handles columns excluded from imputation", {
+  test_data <- tibble::tibble(
+    ID = c(1, 2, 3, 4, 5),
+    A = c(80, 44, NA, 50, 29),  # The 2 neighbors of the 1st col are 44 and 50
+    B = c(30, NA, 85, 70, 54),  # The 2 neighbors of the 2nd col are 30 and 85
+    C = c(7, 10, 25, 74, 49),
+    D = c(14, 0, 5, 9, 20)
+  )
+  result <- impute_median(test_data, exclude_cols = "ID")
+  expected <- tibble::tibble(
+    ID = c(1, 2, 3, 4, 5),
+    A = c(80, 44, 47, 50, 29),
+    B = c(30, 62, 85, 70, 54),
+    C = c(7, 10, 25, 74, 49),
+    D = c(14, 0, 5, 9, 20)
+  )
+  expect_equal(result, expected)
+})
+
+
 # Test impute_knn --------------------------------------------------------------
 test_that("impute_knn imputes values in the proper way", {
   dummy_data <- tibble::tibble(
