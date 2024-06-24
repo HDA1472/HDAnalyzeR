933999e847ee649fb9ea93e5d0d067c97a4b460d
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-06-24 09:55:49 +0200

    Add impute impute_median()

--- R/impute.R
+++ R/impute.R
@@ -1,3 +1,49 @@
+#' Impute via Median
+#'
+#' This function imputes missing values in a dataset using the median of each column.
+#' It allows the user to exclude certain columns from imputation and can also display the
+#' percentage of missing values in each column before imputation.
+#'
+#' @param wide_data (tibble). The input dataframe
+#' @param exclude_cols (string or vector of strings). The columns to exclude from imputation
+#' @param show_na_percentage (TRUE or NULL). If TRUE, the percentage of missing values in each column is displayed
+#'
+#' @return imputed_data (tibble). The dataframe with missing values imputed
+#' @export
+#'
+#' @examples
+#' test_data <- tibble::tibble(
+#'   A = c(80, 44, NA, 50, 29),
+#'   B = c(30, NA, 85, 70, 54),
+#'   C = c(7, 10, 25, 74, 49)
+#' )
+#' imputed_data <- impute_median(test_data)
+impute_median <- function(wide_data, exclude_cols = c("DAid", "Disease"),
+                          show_na_percentage = TRUE) {
+
+  data_in <- wide_data |>
+    dplyr::select(-dplyr::any_of(exclude_cols))
+
+  if (show_na_percentage) {
+    na_percentages <- calc_na_percentage(data_in)
+    print(na_percentages)
+  }
+
+  recipe <- recipes::recipe(~ ., data = data_in) |>
+    recipes::step_impute_median(recipes::all_predictors())
+
+  imputed_data <- recipe |>
+    recipes::prep() |>
+    recipes::bake(new_data = NULL)
+
+  cols <- wide_data |>
+    dplyr::select(dplyr::any_of(exclude_cols))
+  imputed_data <- dplyr::bind_cols(cols, imputed_data)
+
+  return(imputed_data)
+}
+
+
 #' Impute via k-nearest neighbors
 #'
 #' This function imputes missing values in a dataset using the k-nearest neighbors algorithm.
