ecb8c6c3b6959fd8688a702643a8032df60a0cdd
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-06-26 12:35:43 +0200

    Add qc_summary functions create_heatmap(), check_normality() and finish qc_summary_data()

--- R/qc_summary.R
+++ R/qc_summary.R
@@ -1,4 +1,4 @@
-utils::globalVariables(c("DAid", "Assay", "NPX"))
+utils::globalVariables(c("DAid", "Assay", "NPX", "Var1", "Var2", "Freq"))
 #' Calculate the percentage of NAs in each column
 #'
 #' The function calculates the percentage of NAs in each column of the input dataframe.
@@ -51,7 +51,113 @@ calc_na_percentage_row <- function(df) {
 }
 
 
-qc_summary <- function(df, wide = T) {
+#' Check normality of the data
+#'
+#' @param df (tibble). The input dataframe
+#'
+#' @return normality_results (tibble). A tibble with the protein names, p-values, adjusted p-values, and normality status
+#' @export
+#'
+#' @examples
+#' df <- example_data |>
+#'   dplyr::select(DAid, Assay, NPX) |>
+#'   tidyr::pivot_wider(names_from = "Assay", values_from = "NPX")
+#' normality_results <- check_normality(df)
+check_normality <- function(df) {
+
+  future::plan(future::multicore)
+
+  # Perform Shapiro-Wilk test in parallel
+  p_values <- future.apply::future_lapply(df |> dplyr::select(-DAid), function(column) {
+    stats::shapiro.test(column)$p.value
+  })
+
+  adjusted_p_values <- stats::p.adjust(unlist(p_values), method = "BH")
+
+  # Determine normality based on adjusted p-values
+  normality <- adjusted_p_values > 0.05
+
+  normality_results <- tibble::tibble(
+    Protein = names(p_values),
+    p_value = unlist(p_values),
+    adjusted_p_value = adjusted_p_values,
+    is_normal = normality
+  )
+
+  return(normality_results)
+}
+
+
+#' Create a correlation heatmap and report the protein-protein correlations above a threshold
+#'
+#' The function calculates the correlation matrix of the input dataframe.
+#' It filters out the protein pairs with correlation values above or below the threshold.
+#' It then creates a heatmap of the correlation matrix.
+#'
+#' @param df (tibble). The input dataframe
+#' @param threshold (numeric). The reporting protein-protein correlation threshold. Default is 0.8
+#'
+#' @return A list containing the following elements:
+#'   - cor_matrix (matrix). A matrix of protein-protein correlations
+#'   - cor_results (tibble). A tibble with the filtered protein pairs and their correlation values
+#'   - p (ggplot). A heatmap of protein-protein correlations
+#' @export
+#'
+#' @examples
+#' df <- example_data |>
+#'   dplyr::select(DAid, Assay, NPX) |>
+#'   tidyr::pivot_wider(names_from = "Assay", values_from = "NPX")
+#' cor_results <- create_corr_heatmap(df, threshold = 0.7)
+create_corr_heatmap <- function(df, threshold) {
+
+  cor_matrix <- stats::cor(df |> dplyr::select(-DAid),
+                    use = "pairwise.complete.obs",
+                    method = "pearson")
+
+  cor_long <- as.data.frame(as.table(cor_matrix), .name_repair = "minimal")
+
+  cor_results <- cor_long |>
+    dplyr::filter(Var1 != Var2) |>
+    dplyr::filter(Freq > threshold | Freq < -threshold) |>
+    dplyr::arrange(dplyr::desc(Freq)) |>
+    dplyr::rename(Protein1 = Var1, Protein2 = Var2, Correlation = Freq)
+
+  p <- tidyheatmaps::tidyheatmap(cor_long,
+                                 rows = Var1,
+                                 columns = Var2,
+                                 values = Freq,
+                                 cluster_rows = TRUE,
+                                 cluster_cols = TRUE,
+                                 show_selected_row_labels = c(""),
+                                 show_selected_col_labels = c(""),
+                                 color_legend_min = -1,
+                                 color_legend_max = 1,
+                                 treeheight_row = 20,
+                                 treeheight_col = 20
+                                 )
+
+  return(list(cor_matrix = cor_matrix, cor_results = cor_results, p = p))
+}
+
+
+#' Summarize the quality control results of Olink data
+#'
+#' @param df (tibble). The input dataframe
+#' @param wide (logical). Whether the input dataframe is in wide format. Default is TRUE
+#' @param threshold (numeric). The reporting protein-protein correlation threshold. Default is 0.8
+#'
+#' @return A list containing the following elements:
+#'   - na_percentage_col (tibble). A tibble with the column names and the percentage of NAs in each column
+#'   - na_percentage_row (tibble). A tibble with the DAids and the percentage of NAs in each row
+#'   - normality_results (tibble). A tibble with the protein names, p-values, adjusted p-values, and normality status
+#'   - cor_matrix (matrix). A matrix of protein-protein correlations
+#'   - cor_results (tibble). A tibble with the filtered protein pairs and their correlation values
+#'   - heatmap (ggplot). A heatmap of protein-protein correlations
+#' @export
+#'
+#' @examples
+#' qc_summary_data(example_data, wide = FALSE, threshold = 0.7)
+qc_summary_data <- function(df, wide = T, threshold = 0.8) {
 
   if (isFALSE(wide)) {
     df <- df |>
@@ -59,8 +165,24 @@ qc_summary <- function(df, wide = T) {
       tidyr::pivot_wider(names_from = "Assay", values_from = "NPX")
   }
 
+  sample_n <- nrow(df)
+  protein_n <- ncol(df) - 1
   na_percentage_col <- calc_na_percentage_col(df)
   na_percentage_row <- calc_na_percentage_row(df)
+  normality_results <- check_normality(df)
+  cor <- create_corr_heatmap(df, threshold = threshold)
+  cor_matrix <- cor$cor_matrix
+  cor_results <- cor$cor_results
+  p <- cor$p
 
-  return(list(na_percentage_col, na_percentage_row))
+  return(
+    list(
+      na_percentage_col = na_percentage_col,
+      na_percentage_row = na_percentage_row,
+      normality_results = normality_results,
+      cor_matrix = cor_matrix,
+      cor_results = cor_results,
+      heatmap = p
+      )
+    )
 }
