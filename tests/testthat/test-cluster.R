64ddf0f5e531aaf7328583c24eed6040610385ca
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-06-27 17:42:03 +0200

    Add clustering function and respective tests

---
+++ tests/testthat/test-cluster.R
@@ -0,0 +1,56 @@
+# Test impute_median -----------------------------------------------------------
+test_that("cluster_data clusters wide data properly", {
+  test_data <- tibble::tibble(
+    ID = c("A", "B", "C"),
+    protein1 = c(1, 1, 1.6),
+    protein2 = c(0.8, 0.6, 0.5)
+  )
+  result <- cluster_data(test_data)
+  expected <- data.frame(
+    x = c("C", "C", "A", "A", "B", "B"),
+    y = c("protein1", "protein2", "protein1", "protein2", "protein1", "protein2"),
+    value = c(1.6, 0.5, 1.0, 0.8, 1.0, 0.6),
+    stringsAsFactors = TRUE
+  )
+  expected$x <- factor(expected$x, levels = c("C", "A", "B"))
+  expected$y <- factor(expected$y, levels = c("protein1", "protein2"))
+  expect_equal(result, expected)
+})
+
+
+test_that("cluster_data clusters long data properly", {
+  test_data <- tibble::tibble(
+    DAid = c("A", "B", "C", "A", "B", "C"),
+    Assay = c("protein1", "protein1", "protein1", "protein2", "protein2", "protein2"),
+    NPX = c(1, 1, 1.6, 0.8, 0.6, 0.5)
+  )
+  result <- cluster_data(test_data, wide = FALSE)
+  expected <- data.frame(
+    x = c("C", "C", "A", "A", "B", "B"),
+    y = c("protein1", "protein2", "protein1", "protein2", "protein1", "protein2"),
+    value = c(1.6, 0.5, 1.0, 0.8, 1.0, 0.6),
+    stringsAsFactors = TRUE
+  )
+  expected$x <- factor(expected$x, levels = c("C", "A", "B"))
+  expected$y <- factor(expected$y, levels = c("protein1", "protein2"))
+  expect_equal(result, expected)
+})
+
+
+test_that("cluster_data do not cluster rows in wide data", {
+  test_data <- tibble::tibble(
+    ID = c("A", "B", "C"),
+    protein1 = c(1, 1, 1.6),
+    protein2 = c(0.8, 0.6, 0.5)
+  )
+  result <- cluster_data(test_data, cluster_rows = FALSE)
+  expected <- data.frame(
+    x = c("A", "A", "B", "B", "C", "C"),
+    y = c("protein1", "protein2", "protein1", "protein2", "protein1", "protein2"),
+    value = c(1.0, 0.8, 1.0, 0.6, 1.6, 0.5),
+    stringsAsFactors = TRUE
+  )
+  expected$x <- factor(expected$x, levels = c("A", "B", "C"))
+  expected$y <- factor(expected$y, levels = c("protein1", "protein2"))
+  expect_equal(result, expected)
+})
