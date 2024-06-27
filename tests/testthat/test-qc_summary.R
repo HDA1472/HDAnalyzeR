09019f9c439bbbe359f29b68937bbb8ecf02d889
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-06-27 10:05:53 +0200

    Finish qc_summary
    Add print_summary() and qc_summary_metadata()
    Add respective tests
    Update check_nromality() so it arranges the results based on the adjusted p-vals
    Update documentation

--- tests/testthat/test-qc_summary.R
+++ tests/testthat/test-qc_summary.R
@@ -66,8 +66,8 @@ test_that("check_normality estimates normality properly", {
     dplyr::select(Protein, is_normal)
   attr(result$is_normal, "names") <- NULL
   expected <- tibble::tibble(
-    Protein = c("Protein1", "Protein2", "Protein3"),
-    is_normal = c(TRUE, TRUE, FALSE)
+    Protein = c("Protein3", "Protein2", "Protein1"),
+    is_normal = c(FALSE, TRUE, TRUE)
   )
   expect_equal(result, expected)
 })
@@ -114,7 +114,7 @@ test_that("qc_summary_data calculates NA percentages in cols", {
     C = c(7, 10, 25, 74, 49),
     D = c(14, 0, 5, 9, 20)
   )
-  result <- qc_summary_data(test_data)
+  result <- qc_summary_data(test_data, report = FALSE)
   result <- result$na_percentage_col
   expected <- tibble::tibble(
     column = c("A", "B"),
@@ -132,7 +132,7 @@ test_that("qc_summary_data calculates NA percentages in rows", {
     C = c(7, 10, 25, 74, 49),
     D = c(14, 0, 5, 9, 20)
   )
-  result <- qc_summary_data(test_data)
+  result <- qc_summary_data(test_data, report = FALSE)
   result <- result$na_percentage_row
   expected <- tibble::tibble(
     DAid = c("2", "3"),
@@ -150,13 +150,13 @@ test_that("qc_summary_data estimates normality properly", {
     Protein2 = stats::rnorm(100, mean = 30, sd = 5),   # Normally distributed
     Protein3 = stats::rexp(100, rate = 0.1)            # Exponentially distributed (not normal)
   )
-  result <- qc_summary_data(test_data)
+  result <- qc_summary_data(test_data, report = FALSE)
   result <- result$normality_results |>
     dplyr::select(Protein, is_normal)
   attr(result$is_normal, "names") <- NULL
   expected <- tibble::tibble(
-    Protein = c("Protein1", "Protein2", "Protein3"),
-    is_normal = c(TRUE, TRUE, FALSE)
+    Protein = c("Protein3", "Protein2", "Protein1"),
+    is_normal = c(FALSE, TRUE, TRUE)
   )
   expect_equal(result, expected)
 })
@@ -169,7 +169,7 @@ test_that("qc_summary_data returns the correct output matrix", {
     Column2 = c(1, -1, 1, -1, 1, -1, 1, -1, 1, -1),
     Column3 = c(19, 17, 15, 13, 11, 9, 7, 5, 3, 1)
   )
-  result <- qc_summary_data(test_data, threshold = 0.5)
+  result <- qc_summary_data(test_data, threshold = 0.5, report = FALSE)
   result <- result$cor_matrix
   expected <- rbind(c(1, -0.17, -1), c(-0.17, 1, 0.17), c(-1, 0.17, 1))
   rownames(expected) <- colnames(expected) <- c("Column1", "Column2", "Column3")
@@ -184,12 +184,49 @@ test_that("qc_summary_data returns the correct filtered output", {
     Column2 = c(1, -1, 1, -1, 1, -1, 1, -1, 1, -1),
     Column3 = c(19, 17, 15, 13, 11, 9, 7, 5, 3, 1)
   )
-  result <- qc_summary_data(test_data, threshold = 0.5)
+  result <- qc_summary_data(test_data, threshold = 0.5, report = FALSE)
   result <- result$cor_results
   expected <- data.frame(
     Protein1 = c("Column3", "Column1"),
     Protein2 = c("Column1", "Column3"),
     Correlation = c(-1, -1)
+  )
+  expect_equal(result, expected)
+})
+
+
+# Test qc_summary_metadata -----------------------------------------------------
+test_that("qc_summary_metadata calculates NA percentages in cols", {
+  test_metadata <- tibble::tibble(
+    DAid = c("1", "2", "3", "4", "5"),
+    A = c(80, 44, NA, 50, 29),
+    B = c(30, NA, 85, 70, 54),
+    C = c(7, 10, 25, 74, 49),
+    D = c(14, 0, 5, 9, 20)
+  )
+  result <- qc_summary_metadata(test_metadata, report = FALSE)
+  result <- result$na_percentage_col
+  expected <- tibble::tibble(
+    column = c("A", "B"),
+    na_percentage = c(20.0, 20.0)
+  )
+  expect_equal(result, expected)
+})
+
+
+test_that("qc_summary_metadata calculates NA percentages in rows", {
+  test_metadata <- tibble::tibble(
+    DAid = c("1", "2", "3", "4", "5"),
+    A = c(80, 44, NA, 50, 29),
+    B = c(30, NA, 85, 70, 54),
+    C = c(7, 10, 25, 74, 49),
+    D = c(14, 0, 5, 9, 20)
+  )
+  result <- qc_summary_metadata(test_metadata, report = FALSE)
+  result <- result$na_percentage_row
+  expected <- tibble::tibble(
+    DAid = c("2", "3"),
+    na_percentage = c(20.0, 20.0)
   )
   expect_equal(result, expected)
 })
