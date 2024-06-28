920251294604e0e0360a2a713311032b0d3e0e77
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-06-28 14:12:23 +0200

    Create correlate with correlate() and move function create_corr_heatmap() from qc_summary. Add respective tests

---
+++ R/correlate.R
@@ -0,0 +1,91 @@
+utils::globalVariables(c("Var1", "Var2", "Freq"))
+#' Calculate the correlation matrix of the input dataframe
+#'
+#' @param x (vector or tibble). A numeric vector, matrix or data frame.
+#' @param y (vector, tibble or NULL). A numeric vector, matrix or data frame with compatible dimensions with `x`. Default is NULL
+#' @param use (character). A character string. The method to use for computing correlations. Default is "pairwise.complete.obs"
+#' @param method (character). A character string. The correlation method to use. Default is "pearson"
+#'
+#' @return cor_matrix (matrix). A matrix of protein-protein correlations
+#' @export
+#'
+#' @examples
+#' df <- example_data |>
+#'   dplyr::select(DAid, Assay, NPX) |>
+#'   tidyr::pivot_wider(names_from = "Assay", values_from = "NPX") |>
+#'   dplyr::select(-DAid)
+#' cor_results <- correlate(df)
+correlate <- function(x, y = NULL, use = "pairwise.complete.obs", method = "pearson") {
+
+  cor_matrix <- round(
+    stats::cor(x, y, use = "pairwise.complete.obs", method = "pearson"),
+    2
+  )
+
+  return(cor_matrix)
+}
+
+
+#' Create a correlation heatmap and report the protein-protein correlations above a threshold
+#'
+#' The function calculates the correlation matrix of the input dataframe.
+#' It filters out the protein pairs with correlation values above or below the threshold.
+#' It then creates a heatmap of the correlation matrix.
+#'
+#' @param x (vector or tibble). A numeric vector, matrix or data frame.
+#' @param y (vector, tibble or NULL). A numeric vector, matrix or data frame with compatible dimensions with `x`. Default is NULL
+#' @param use (character). A character string. The method to use for computing correlations. Default is "pairwise.complete.obs"
+#' @param method (character). A character string. The correlation method to use. Default is "pearson"
+#' @param threshold (numeric). The reporting protein-protein correlation threshold. Default is 0.8
+#' @param cluster_rows (logical). Whether to cluster the rows. Default is TRUE
+#' @param cluster_cols (logical). Whether to cluster the columns. Default is TRUE
+#'
+#' @return A list containing the following elements:
+#'   - cor_matrix (matrix). A matrix of protein-protein correlations
+#'   - cor_results (tibble). A tibble with the filtered protein pairs and their correlation values
+#'   - p (plot). A heatmap of protein-protein correlations
+#' @export
+#'
+#' @examples
+#' df <- example_data |>
+#'   dplyr::select(DAid, Assay, NPX) |>
+#'   tidyr::pivot_wider(names_from = "Assay", values_from = "NPX") |>
+#'   dplyr::select(-DAid)
+#' cor_results <- create_corr_heatmap(df, threshold = 0.7)
+create_corr_heatmap <- function(x,
+                                y = NULL,
+                                use = "pairwise.complete.obs",
+                                method = "pearson",
+                                threshold = 0.8,
+                                cluster_rows = TRUE,
+                                cluster_cols = TRUE) {
+
+  cor_matrix <- correlate(x,
+                          y = NULL,
+                          use = "pairwise.complete.obs",
+                          method = "pearson")
+
+  cor_long <- as.data.frame(as.table(cor_matrix), .name_repair = "minimal", stringsAsFactors = FALSE)
+
+  cor_results <- cor_long |>
+    dplyr::filter(Var1 != Var2) |>
+    dplyr::filter(Freq > threshold | Freq < -threshold) |>
+    dplyr::arrange(dplyr::desc(Freq)) |>
+    dplyr::rename(Protein1 = Var1, Protein2 = Var2, Correlation = Freq)
+
+  p <- tidyheatmaps::tidyheatmap(cor_long,
+                                 rows = Var1,
+                                 columns = Var2,
+                                 values = Freq,
+                                 cluster_rows = TRUE,
+                                 cluster_cols = TRUE,
+                                 show_selected_row_labels = c(""),
+                                 show_selected_col_labels = c(""),
+                                 color_legend_min = -1,
+                                 color_legend_max = 1,
+                                 treeheight_row = 20,
+                                 treeheight_col = 20
+  )
+
+  return(list(cor_matrix = cor_matrix, cor_results = cor_results, p = p))
+}
--- R/qc_summary.R
+++ R/qc_summary.R
@@ -1,4 +1,4 @@
-utils::globalVariables(c("DAid", "Assay", "NPX", "adjusted_p_value", "Var1", "Var2", "Freq"))
+utils::globalVariables(c("DAid", "Assay", "NPX", "adjusted_p_value"))
 #' Calculate the percentage of NAs in each column
 #'
 #' The function calculates the percentage of NAs in each column of the input dataframe.
@@ -86,61 +86,6 @@ check_normality <- function(df) {
     dplyr::arrange((adjusted_p_value))
 
   return(normality_results)
-}
-
-
-#' Create a correlation heatmap and report the protein-protein correlations above a threshold
-#'
-#' The function calculates the correlation matrix of the input dataframe.
-#' It filters out the protein pairs with correlation values above or below the threshold.
-#' It then creates a heatmap of the correlation matrix.
-#'
-#' @param df (tibble). The input dataframe
-#' @param threshold (numeric). The reporting protein-protein correlation threshold. Default is 0.8
-#'
-#' @return A list containing the following elements:
-#'   - cor_matrix (matrix). A matrix of protein-protein correlations
-#'   - cor_results (tibble). A tibble with the filtered protein pairs and their correlation values
-#'   - p (plot). A heatmap of protein-protein correlations
-#' @export
-#'
-#' @examples
-#' df <- example_data |>
-#'   dplyr::select(DAid, Assay, NPX) |>
-#'   tidyr::pivot_wider(names_from = "Assay", values_from = "NPX")
-#' cor_results <- create_corr_heatmap(df, threshold = 0.7)
-create_corr_heatmap <- function(df, threshold) {
-
-  cor_matrix <- round(
-    stats::cor(df |> dplyr::select(-dplyr::any_of(c("DAid"))),
-               use = "pairwise.complete.obs",
-               method = "pearson"),
-    2
-  )
-
-  cor_long <- as.data.frame(as.table(cor_matrix), .name_repair = "minimal", stringsAsFactors = FALSE)
-
-  cor_results <- cor_long |>
-    dplyr::filter(Var1 != Var2) |>
-    dplyr::filter(Freq > threshold | Freq < -threshold) |>
-    dplyr::arrange(dplyr::desc(Freq)) |>
-    dplyr::rename(Protein1 = Var1, Protein2 = Var2, Correlation = Freq)
-
-  p <- tidyheatmaps::tidyheatmap(cor_long,
-                                 rows = Var1,
-                                 columns = Var2,
-                                 values = Freq,
-                                 cluster_rows = TRUE,
-                                 cluster_cols = TRUE,
-                                 show_selected_row_labels = c(""),
-                                 show_selected_col_labels = c(""),
-                                 color_legend_min = -1,
-                                 color_legend_max = 1,
-                                 treeheight_row = 20,
-                                 treeheight_col = 20
-                                 )
-
-  return(list(cor_matrix = cor_matrix, cor_results = cor_results, p = p))
 }
 
 
@@ -233,7 +178,8 @@ qc_summary_data <- function(df, wide = T, threshold = 0.8, report = T) {
   na_percentage_col <- calc_na_percentage_col(df)
   na_percentage_row <- calc_na_percentage_row(df)
   normality_results <- check_normality(df)
-  cor <- create_corr_heatmap(df, threshold = threshold)
+  cor <- create_corr_heatmap(df |> dplyr::select(-dplyr::any_of(c("DAid"))),
+                             threshold = threshold)
   cor_matrix <- cor$cor_matrix
   cor_results <- cor$cor_results
   p <- cor$p
