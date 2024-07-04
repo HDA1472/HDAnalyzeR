d96cf0b987c5761d5e0b84b9aedd35db5fb56835
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-07-04 18:43:23 +0200

    Add Maria's feedback to clustering
    - Prepare to create dendrograms and create them with internal function
    - We will have though to create a heatmap function with these dendrograms in the future

--- R/cluster.R
+++ R/cluster.R
@@ -11,7 +11,10 @@ utils::globalVariables(c("v1", "v2", "val", "x", "y"))
 #' @param cluster_cols (logical). Whether to cluster columns. Default is TRUE.
 #' @param wide (logical). Whether the data is wide or long. Default is TRUE.
 #'
-#' @return clustering_results (data.frame). A data frame with the clustered data.
+#' @return (list). A list with the following elements:
+#'  - clustered_data (tibble). A data frame with the clustered data.
+#'  - hc_rows (hclust). The hierarchical clustering object for rows.
+#'  - hc_cols (hclust). The hierarchical clustering object for columns.
 #' @export
 #'
 #' @examples
@@ -27,21 +30,23 @@ cluster_data <- function(df, distance_method = "euclidean", clustering_method = 
     order_col <- colnames(wide_data)
 
     if(isTRUE(cluster_rows)) {
-      order1 <- wide_data |>
+      hc_rows <- wide_data |>
         stats::dist(method = distance_method) |>
-        stats::hclust(method = clustering_method) |>
-        with(labels[order])
+        stats::hclust(method = clustering_method)
+      order1 <- hc_rows$labels[hc_rows$order]
     } else {
+      hc_rows <- NULL
       order1 <- order_row
     }
 
     if(isTRUE(cluster_cols)) {
-      order2 <- wide_data |>
+      hc_cols <- wide_data |>
         t() |>
         stats::dist(method = distance_method) |>
-        stats::hclust(method = clustering_method) |>
-        with(labels[order])
+        stats::hclust(method = clustering_method)
+      order2 <- hc_cols$labels[hc_cols$order]
     } else {
+      hc_cols <- NULL
       order2 <- order_col
     }
 
@@ -54,5 +59,32 @@ cluster_data <- function(df, distance_method = "euclidean", clustering_method = 
                     y = factor(y, levels = order2)) |>
       dplyr::arrange(x, y)
 
-    return(clustering_results)
+    clustered_data <- tibble::as_tibble(clustering_results)
+    return(list("clustered_data" = clustered_data, "hc_rows" = hc_rows, "hc_cols" = hc_cols))
+}
+
+
+#' Create dendrograms (under development - it should be inside a ggplot heatmap function)
+#'
+#' This function creates dendrograms for the rows and columns of the clustered data.
+#'
+#' @param hc_rows (hclust). The hierarchical clustering object for rows.
+#' @param hc_cols (hclust). The hierarchical clustering object for columns.
+#'
+#' @return (list). A list with the following elements:
+#'  - dend_rows (ggplot). A ggplot object with the dendrogram for rows.
+#'  - dend_cols (ggplot). A ggplot object with the dendrogram for columns.
+#' @keywords internal
+create_dendrograms <- function(hc_rows, hc_cols) {
+  if (!is.null(hc_rows)) {
+    dend_rows <- ggdendro::ggdendrogram(hc_rows, labels = F, leaf_labels = F, rotate = T) +
+      ggplot2::scale_y_reverse() +
+      ggdendro::theme_dendro()
+  }
+
+  if (!is.null(hc_cols)) {
+    dend_cols <- ggdendro::ggdendrogram(hc_cols, labels = F, leaf_labels = F) +
+      ggdendro::theme_dendro()
+  }
+    return(list("dend_rows" = dend_rows, "dend_cols" = dend_cols))
 }
