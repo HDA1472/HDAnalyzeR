37ef4f7fb0466d26209b55b91d27d1c7beb26d41
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-07-16 14:58:31 +0200

    Update palettes and add themes

---
+++ R/themes.R
@@ -0,0 +1,52 @@
+theme_hpa <- function(angled = F,
+                      axis_x = T,
+                      axis_y = T,
+                      facet_title = T) {
+
+    t <- ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
+               panel.grid.minor = ggplot2::element_blank(),
+               panel.spacing = ggplot2::unit(0.2, "lines"),
+               panel.background = ggplot2::element_rect(fill = "white"),
+               panel.border = ggplot2::element_blank(),
+               plot.title = ggplot2::element_text(face = "bold",
+                                         size = ggplot2::rel(1),
+                                         hjust = 0.5),
+               plot.subtitle = ggplot2::element_text(face = "bold",
+                                            hjust = 0.5,
+                                            size = ggplot2::rel(1),
+                                            vjust = 1),
+               axis.title = ggplot2::element_text(face = "bold", size = ggplot2::rel(1)),
+               axis.ticks.length = ggplot2::unit(.25, "cm"),
+               axis.line = ggplot2::element_line(linewidth = 0.5),
+               axis.text = ggplot2::element_text(size = ggplot2::rel(1), color = 'black'),
+               legend.key = ggplot2::element_blank(),
+               legend.position = "right",
+               legend.text = ggplot2::element_text(size = ggplot2::rel(0.8)),
+               legend.key.size = ggplot2::unit(0.7, "cm"),
+               legend.title = ggplot2::element_text(size = ggplot2::rel(1)),
+               plot.margin = ggplot2::unit(c(10, 5, 5, 5), "mm"),
+               strip.background = ggplot2::element_rect(colour = "grey90", fill = "grey90"),
+               strip.text = ggplot2::element_text(face = "bold"))
+
+    if (angled) {
+      t <- t + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1))
+    }
+
+    if (axis_x == F) {
+      t <- t + ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                     axis.ticks.x = ggplot2::element_blank(),
+                     axis.line.x = ggplot2::element_blank(),
+                     axis.title.x = ggplot2::element_blank())
+    }
+
+    if (axis_y == F) {
+      t <- t + ggplot2::theme(axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank(),
+                     axis.line.y = ggplot2::element_blank(),
+                     axis.title.y = ggplot2::element_blank())
+    }
+    if (facet_title == F) {
+      t <- t + ggplot2::theme(strip.text = ggplot2::element_blank())
+    }
+    return(t)
+  }
