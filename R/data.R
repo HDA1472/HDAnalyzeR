0caa277d13e867b9c255165846870859062c5e57
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-07-23 15:07:17 +0200

    Update data and metadata docs

--- R/data.R
+++ R/data.R
@@ -1,22 +1,24 @@
-#' Example cancer data
+#' Cancer cohort Olink data
 #'
-#' A subset of data from a synthetic cancer dataset. Only the first 100 Assays.
-#' With DAid, Assay_Warning, QC_Warning, and PlateID as extra columns.
+#' A data subset from a synthetic cancer dataset keeping the first 100 Assays.
+#' DAid, Assay_Warning, QC_Warning, and PlateID have been added as extra columns.
 #' The original dataset was processed with the `process_example_data` script.
 #'
-#' @format ## `example_data`
-#' A data frame with 56,142 rows and 10 columns:
+#' @source <https://github.com/buenoalvezm/Pan-cancer-profiling/blob/main/data/cancer_data_synthetic.rds>
+#' @format
+#' A tibble with 56,142 rows and 10 columns:
 #' \describe{
 #'   \item{DAid}{The Disease Atlas sample ID}
 #'   \item{Sample}{The Sample ID}
-#'   \item{OlinkID}{The Olink ID of the Assay}
-#'   \item{UniProt}{The UniProt ID of the Assay}
+#'   \item{OlinkID}{The Olink Assay ID}
+#'   \item{UniProt}{The UniProt Assay ID}
 #'   \item{Assay}{The Assay name}
-#'   \item{Panel}{The Olink Panel in which the Assay belongs}
+#'   \item{Panel}{The Olink Panel in which the Assay belongs to}
 #'   \item{NPX}{The NPX value for each Sample and Assay}
 #'   \item{Assay_Warning}{The Assay warning status for the sample}
 #'   \item{QC_Warning}{The QC warning status for the sample}
 #'   \item{PlateID}{The ID of the plate where the sample was processed}
 #' }
-#' @source <https://github.com/buenoalvezm/Pan-cancer-profiling/blob/main/data/cancer_data_synthetic.rds>
+#' @examples
+#' example_data
 "example_data"
--- R/metadata.R
+++ R/metadata.R
@@ -1,14 +1,16 @@
-#' Example cancer metadata
+#' Cancer cohort metadata
 #'
-#' A subset of metadata from a synthetic cancer dataset. With DAid, Age, BMI, and Cohort  as extra
-#' columns. The original dataset was processed with the `process_example_metadata` script.
+#' A data subset from synthetic cancer metadata. DAid, Age, BMI, and Cohort have
+#' been added as extra columns. The original dataset was processed with the
+#' `process_example_metadata` script.
 #'
-#' @format ## `example_metadata`
-#' A data frame with 586 rows and 9 columns:
+#' @source <https://github.com/buenoalvezm/Pan-cancer-profiling/blob/main/data/cancer_metadata_synthetic.rds>
+#' @format
+#' A tibble with 586 rows and 9 columns:
 #' \describe{
 #'   \item{DAid}{The Disease Atlas sample ID}
 #'   \item{Sample}{The Sample ID}
-#'   \item{GROUP}{The cancer type}
+#'   \item{Disease}{The cancer type}
 #'   \item{Stage}{The cancer stage}
 #'   \item{Grade}{The cancer grade}
 #'   \item{Sex}{The patient sex}
@@ -16,5 +18,6 @@
 #'   \item{BMI}{The patient BMI}
 #'   \item{Cohort}{The cohort the patient belongs to}
 #' }
-#' @source <https://github.com/buenoalvezm/Pan-cancer-profiling/blob/main/data/cancer_metadata_synthetic.rds>
+#' @examples
+#' example_metadata
 "example_metadata"
