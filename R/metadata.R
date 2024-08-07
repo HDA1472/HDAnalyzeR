52350f3a6b91c68c071b4d7a9a49464a08286f1c
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-08-07 12:22:43 +0200

    Move Cohort column back to metadata after discussion with Maria

--- R/metadata.R
+++ R/metadata.R
@@ -1,12 +1,12 @@
 #' Cancer cohort metadata
 #'
-#' A data subset from synthetic cancer metadata. DAid, Age, and BMI have
+#' A data subset from synthetic cancer metadata. DAid, Age, BMI, and Cohort have
 #' been added as extra columns. The original dataset was processed with the
 #' `process_example_metadata` script.
 #'
 #' @source <https://github.com/buenoalvezm/Pan-cancer-profiling/blob/main/data/cancer_metadata_synthetic.rds>
 #' @format
-#' A tibble with 586 rows and 8 columns:
+#' A tibble with 586 rows and 9 columns:
 #' \describe{
 #'   \item{DAid}{The Disease Atlas sample ID}
 #'   \item{Sample}{The Sample ID}
@@ -16,6 +16,7 @@
 #'   \item{Sex}{The patient sex}
 #'   \item{Age}{The patient age}
 #'   \item{BMI}{The patient BMI}
+#'   \item{Cohort}{The cohort the patient belongs to}
 #' }
 #' @examples
 #' example_metadata
