c1f409a26bdcd363450905fcbd6a70de6358393d
Konstantinos Antonopoulos <k.antono@outlook.com>
2024-09-06 16:13:57 +0200

    Add new function for plotting heatmap summary of biomarkers

--- R/visualize.R
+++ R/visualize.R
@@ -172,3 +172,153 @@ plot_scatter_with_regression <- function(plot_data,
 
   return(scatter)
 }
+
+
+#' Plot a summary heatmap of the combined differential expression and classification models results
+#'
+#' `plot_biomarkers_summary_heatmap` plots a summary heatmap of the combined differential
+#' expression and classification models results. The heatmap shows the log2 fold change
+#' and adjusted p-value of the differential expression results, and the scaled importance
+#' and sign of the classification models results. The heatmap is ordered and the selected
+#' assays are based on the specified control group.
+#'
+#' @param de_results A list of differential expression results.
+#' @param ml_results A list of classification models results.
+#' @param order_by The control group to order the heatmap.
+#' @param pval_lim The p-value limit to filter the differential expression results of the `order_by` group.
+#' @param logfc_lim The log2 fold change limit to filter the differential expression results  of the `order_by` group.
+#'
+#' @return The summary heatmap of the combined differential expression and classification models results.
+#' @export
+#'
+#' @details It is very important the de_results and ml_results are in the same order
+#' and in the right format (see examples).
+#'
+#' @examples
+#' # Prepare differential expression results
+#' de_results_amlbrc <- do_limma(example_data,
+#'                               example_metadata,
+#'                               case = "AML",
+#'                               control = c("BRC"),
+#'                               wide = FALSE,
+#'                               only_female = "BRC")
+#'
+#' de_results_amlcll <- do_limma(example_data,
+#'                               example_metadata,
+#'                               case = "AML",
+#'                               control = c("CLL"),
+#'                               wide = FALSE,
+#'                               only_female = "BRC")
+#'
+#' de_results_amllungc <- do_limma(example_data,
+#'                                 example_metadata,
+#'                                 case = "AML",
+#'                                 control = c("LUNGC"),
+#'                                 wide = FALSE,
+#'                                 only_female = "BRC")
+#'
+#' # Combine the results
+#' res_de <- list("BRC" = de_results_amlbrc,
+#'                "CLL" = de_results_amlcll,
+#'                "LUNGC" = de_results_amllungc)
+#'
+#' res_amlbrc <- do_rreg(example_data,
+#'                       example_metadata,
+#'                       case = "AML",
+#'                       type = 'elnet',
+#'                       control = c("BRC"),
+#'                       wide = FALSE,
+#'                       only_female = "BRC",
+#'                       cv_sets = 2,
+#'                       grid_size = 1,
+#'                       ncores = 1)
+#'
+#' res_amlcll <- do_rreg(example_data,
+#'                       example_metadata,
+#'                       case = "AML",
+#'                       type = 'elnet',
+#'                       control = c("CLL"),
+#'                       wide = FALSE,
+#'                       only_female = "BRC",
+#'                       cv_sets = 2,
+#'                       grid_size = 1,
+#'                       ncores = 1)
+#'
+#' res_amllungc <- do_rreg(example_data,
+#'                         example_metadata,
+#'                         case = "AML",
+#'                         control = c("LUNGC"),
+#'                         type = 'elnet',
+#'                         wide = FALSE,
+#'                         only_female = "BRC",
+#'                         cv_sets = 2,
+#'                         grid_size = 1,
+#'                         ncores = 1)
+#'
+#' # Combine the results
+#' res_ml <- list("BRC" = res_amlbrc,
+#'                "CLL" = res_amlcll,
+#'                "LUNGC" = res_amllungc)
+#'
+#' # Create the summary heatmap
+#' plot_biomarkers_summary_heatmap(res_de, res_ml, order_by = "BRC")
+plot_biomarkers_summary_heatmap <- function(de_results,
+                                            ml_results,
+                                            order_by=NULL,
+                                            pval_lim = 0.05,
+                                            logfc_lim = 0) {
+
+  res_de <- de_results[[order_by]]$de_results |>
+    dplyr::filter(abs(logFC) > logfc_lim, adj.P.Val < pval_lim) |>
+    dplyr::select(Assay, logFC, adj.P.Val)
+
+  assays <- res_de$Assay
+
+  res_plot <- tibble::tibble()
+  for (i in 1:length(de_results)) {
+
+    res_de <- de_results[[i]]$de_results |>
+      dplyr::filter(Assay %in% assays) |>
+      dplyr::select(Assay, logFC, adj.P.Val)
+
+    res_ml <- ml_results[[i]]$var_imp_res$features |>
+      dplyr::rename(Assay = Variable) |>
+      dplyr::filter(Assay %in% assays) |>
+      dplyr::select(Assay, Scaled_Importance, Sign)
+
+    res_combined <- res_de |>
+      dplyr::left_join(res_ml, by = c("Assay")) |>
+      dplyr::mutate(control_group = names(de_results)[[i]]) |>
+      dplyr::arrange(logFC)
+
+    res_plot <- rbind(res_plot, res_combined)
+    }
+
+  summary_heatmap <- res_plot |>
+    dplyr::mutate(Assay = factor(Assay, levels = res_plot |>
+                                   dplyr::filter(control_group == order_by) |>
+                                   dplyr::arrange(logFC) |>
+                                   dplyr::pull(Assay))) |>
+    ggplot2::ggplot(ggplot2::aes(x = Assay, y = control_group)) +
+    ggplot2::geom_tile(ggplot2::aes(fill = logFC), color = "white") +
+    ggplot2::geom_point(ggplot2::aes(size = Scaled_Importance, color = Sign)) +
+    ggplot2::geom_point(ggplot2::aes(size = Scaled_Importance), shape = 1, colour = "black") +
+    ggplot2::geom_text(ggplot2::aes(label = ifelse(adj.P.Val < pval_lim, "*", "")), color = "black", size = 2) +
+    ggplot2::scale_fill_gradient2(low = "cornflowerblue", mid = "white", high = "brown4", midpoint = 0, name = "Log2 FC") +
+    ggplot2::scale_color_manual(values = c("POS" = "cornflowerblue", "NEG" = "brown4"), name = "Sign", na.translate = FALSE) +
+    ggplot2::scale_size(name = "Scaled Importance") +
+    ggplot2::labs(x = "Protein (Assay)", y = "Control Group") +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1),
+                   axis.text.y = ggplot2::element_text(),
+                   axis.title.x = ggplot2::element_text(face = "bold", size = 12),
+                   axis.title.y = ggplot2::element_text(face = "bold", size = 12),
+                   panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   legend.position = "top",
+                   legend.box = "horizontal",
+                   legend.title = ggplot2::element_text(size = 10),
+                   legend.text = ggplot2::element_text(size = 9))
+
+  return(summary_heatmap)
+}
